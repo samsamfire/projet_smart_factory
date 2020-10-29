a08886b060302def2313f2977b87e20a77e9a0eb
samsam <samfire.pilote@gmail.com>
2020-10-29 18:54:00 +0100

    -Merged and also added /install to gitignore

---
+++ src/agv_controller/include/agv_controller/agv_hw.h
@@ -0,0 +1,43 @@
+
+#include <hardware_interface/joint_command_interface.h>
+#include "hardware_interface/posvel_command_interface.h"
+#include <hardware_interface/joint_state_interface.h>
+#include <hardware_interface/robot_hw.h>
+
+
+
+//Class descends from RobotHW from hardware_interface lib
+//Agv has 4 actuators (4 motoros)
+
+
+
+
+class Agv : public hardware_interface::RobotHW
+{
+	
+public:
+
+	Agv();
+	void setVel(const double * cmd_vel[4]);
+	void setPos(const double * cmd_pos[4]);
+	double * getVel();
+	double * getPos();
+	
+
+private:
+	//Create jointstateinterface object
+	hardware_interface::JointStateInterface jnt_state_interface;
+	hardware_interface::PositionJointInterface jnt_pos_interface;
+	hardware_interface::PosVelJointInterface jnt_pos_vel_interface;
+
+
+	double cmd_pos[4]; //Command position
+	double cmd_vel[4]; //Comand speed
+	
+	double pos[4]; //Position of all 4 actuators
+	double vel[4]; //Speed of all 4 actuators
+	double eff[4]; //Effort of all 4 actuators (Measure with current ??)
+
+};
+
+ 
---
+++ src/agv_controller/src/agv_controller_sim.cpp
@@ -0,0 +1,30 @@
+#include "../include/agv_controller/agv_controller_sim.h"
+#include "../include/agv_controller/agv_hw.h"
+#include "ros/ros.h"
+
+
+
+
+/*Controller simulation for ros inside Gazebo (or rviz todo) */
+
+
+
+
+int main(int argc, char const *argv[])
+{
+	
+
+
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
